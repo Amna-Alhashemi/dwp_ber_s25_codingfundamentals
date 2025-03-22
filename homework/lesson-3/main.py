b3e599dec372f359374c8ff9cada74f488d03227
Amna-Alhashemi <amnahashemi@gmail.com>
2025-03-22 12:23:29 +0000

    Conditions, loops

--- homework/lesson-3/main.py
+++ homework/lesson-3/main.py
@@ -1,9 +1,27 @@
+# Conditions 
 score = 85
-if score > 90: 
-    print ("A")
-elif score > 80 and score <=90:
-    print ("B")
-elif score >= 60 and score <=80: 
-    print ("C")
-else:
-    print("D")
+#if score > 90: 
+    # print ("A")
+#elif score > 80 and score <=90:
+    # print ("B")
+#elif score >= 60 and score <=80: 
+    # print ("C")
+#else:
+    # print("D")
+
+# Loops
+number = 0 
+#while number <= 100:
+   # print (number)
+#    number = number + 1
+
+for count in range (0, 100):
+    if count %2 != 0:
+        print(count)
+    # print (count)
+
+# Inputs: 
+my_name = input("What is your name?")
+print ("Hello", my_name)
+    
+    
