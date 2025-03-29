86d391836373f4e88cda272bce559ef7ad35dcd0
Amna-Alhashemi <amnahashemi@gmail.com>
2025-03-29 12:26:07 +0000

    Lesson 4

---
+++ homework/lesson-4/main.py
@@ -0,0 +1,43 @@
+
+# scores = [15, 19, 17, 12, 17, 13]
+# ## print the first score (by accessing it from the list)
+# print (scores[0])
+# ## print the last score (by accessing it from the list)
+# print (scores[-1])
+# ## print the highest score (by accessing it from the list) (Hint: max(example_list))
+# print(max(scores))
+# ## print the lowest score (by accessing it from the list) (Hint: min(example_list))
+# print(min(scores))
+# ## add 21 to the list of scores (Hint: append(number_to_include))
+# scores.append(21)
+# print(scores)
+# ## sort the scores in increasing order (Hint: sort())
+# # scores.sort()
+# print(scores)
+# # bonus: remove one of the 17 (Hint: remove(number_to_remove), pop(index))
+# # scores.remove(17)
+# scores.pop(2)
+# print(scores)
+
+
+# groceries = set()
+
+# groceries = {}
+# groceries = {'Tomatoes', 'Carrots'}
+
+# fruits = ['Banana', 'Mango']
+# for fruit in fruits: 
+#     print (fruit)
+
+# scores = [12, 45, 35]
+
+# for s in scores: 
+#     print(s*2)
+
+# double_scores = []
+
+# for s in scores: 
+#     double_scores.append(s*2)
+   
+# print(double_scores)
+
